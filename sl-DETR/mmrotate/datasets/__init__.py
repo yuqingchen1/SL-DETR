f3fecc4339da074eb66413a32bd65ee6d1fe34a8
yuqingchen <108913257+yuqingchen1@users.noreply.github.com>
2024-10-06 10:54:49 +0800

    Add files via upload

---
+++ sl-DETR/mmrotate/datasets/__init__.py
@@ -0,0 +1,15 @@
+# Copyright (c) SI Analytics. All rights reserved.
+# Licensed under the CC BY-NC 4.0 License. See LICENSE file in the project root for full license information.
+#
+# Copyright (c) OpenMMLab. All rights reserved.
+# Licensed under the Apache License, Version 2.0. See LICENSE file in the mmrotate repository for full license information.
+from .dior import DIORDataset  # noqa: F401, F403
+from .dota import DOTAv2Dataset  # noqa: F401, F403
+from .dota import DOTADataset, DOTAv15Dataset
+from .hrsc import HRSCDataset  # noqa: F401, F403
+from .transforms import *  # noqa: F401, F403
+
+__all__ = [
+    'DOTADataset', 'DOTAv15Dataset', 'DOTAv2Dataset', 'HRSCDataset',
+    'DIORDataset'
+]
